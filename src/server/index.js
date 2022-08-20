438a6709fe0599bb3ac7780a45e59acfd7564007
Yohannes Hailu <yohanneshdev21@gmail.com>
2022-08-20 22:00:19 +0300

    backend routes, controllers and models are created

---
+++ src/server/index.js
@@ -0,0 +1,31 @@
+//for using ES Modules instead of CommonJS, the type property must be specified with a value of "module"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
+
+const app = express();
+dotenv.config();
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("connected to MongoDB");
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+
+app.listen(8800, () => {
+  connect();
+  console.log("connected to server");
+});
---
+++ src/server/models/Comment.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    videoId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+//Comment collection will be changed to "Comments" by default by mongoDB
+export default mongoose.model("Comment", CommentSchema);
