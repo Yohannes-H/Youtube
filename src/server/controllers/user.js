b45ac92b540d64252e2326a7992fd82fc6993812
Yohannes Hailu <yohanneshdev21@gmail.com>
2022-08-22 22:12:43 +0300

    Video Controllers are updated

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -20,9 +20,64 @@ export const update = async (req, res, next) => {
     return next(createError(403, "Ypu can only update your account!!!"));
   }
 };
-export const deleteUser = (req, res, next) => {};
-export const getUser = (req, res, next) => {};
-export const subscribe = (req, res, next) => {};
-export const unsubscribe = (req, res, next) => {};
-export const like = (req, res, next) => {};
-export const dislike = (req, res, next) => {};
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+
+      res.status(200).json("user has been deleted!!!");
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(403, "Ypu can only delete your account!!!"));
+  }
+};
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+export const subscribe = async (req, res, next) => {
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      $push: { subscribedUsers: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: 1 },
+    });
+
+    res.status(200).json("subscription successful");
+  } catch (err) {
+    next(err);
+  }
+};
+export const unsubscribe = async (req, res, next) => {
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      $pull: { subscribedUsers: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: -1 },
+    });
+
+    res.status(200).json("unsubscription successful");
+  } catch (err) {
+    next(err);
+  }
+};
+export const like = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+export const dislike = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
--- src/server/routes/videos.js
+++ src/server/routes/videos.js
@@ -1,6 +1,16 @@
 import express from "express";
-import {} from "../controllers/video.js";
+import { addVideo, addView, random, sub, trend } from "../controllers/video.js";
+import { verifyToken } from "../VerifyToken.js";
 
 const router = express.Router();
+
+router.post("/", verifyToken, addVideo);
+router.put("/:id", verifyToken, addVideo);
+router.delete("/:id", verifyToken, addVideo);
+router.get("/find/:id", addVideo);
+router.put("/view/:id", addView);
+router.get("/trend", trend);
+router.get("/random", random);
+router.get("/sub", verifyToken, sub);
 
 export default router;
