e30139d83d542af1a37316201d3dd5206cf678d9
Yohannes Hailu <yohanneshdev21@gmail.com>
2022-08-23 10:23:49 +0300

    Like and Dislike controlers are updated

--- src/server/routes/comments.js
+++ src/server/routes/comments.js
@@ -1,6 +1,15 @@
 import express from "express";
-import {} from "../controllers/comment.js";
+import {
+  addComment,
+  deleteComment,
+  getComments,
+} from "../controllers/comment.js";
+import { verifyToken } from "../VerifyToken.js";
 
 const router = express.Router();
+
+router.post("/", verifyToken, addComment);
+router.delete("/:id", verifyToken, deleteComment);
+router.post("/:videoId", getComments);
 
 export default router;
